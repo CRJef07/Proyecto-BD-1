b101829632e92a0c947506a764edca316fc309a7
Jefté Andrés Vega Hidalgo <jefte.vega.hidalgo@est.una.ac.cr>
2022-06-12 15:29:41 -0600

    Primer commit
    Se inicializa el repositorio con el proyecto

---
+++ src/condominiosvac/CondominiosVAC.java
@@ -0,0 +1,10 @@
+package condominiosvac;
+
+/* @author Jefte Vega, Bryan Abarca, Hillary Cruz */
+public class CondominiosVAC {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+    }
+
+}
